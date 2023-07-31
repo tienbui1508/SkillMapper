e1f5e8d1a0ed5e2badfae702d1664b79dc3749e0
tienbui1508 <tienbui1508@gmail.com>
2023-07-31 20:53:51 +0700

    Initial Commit

---
+++ SkillMapper/SkillMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillMapperApp.swift
+//  SkillMapper
+//
+//  Created by Tien Bui on 31/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SkillMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
