4acb65dbca83c0f2b1cfe15561164f7e6a5e5bd4
tienbui <tienbui1508@gmail.com>
2023-08-31 21:37:14 +0930

    Add code to determine which awards have actually been earned.

--- SkillMapper/DataController.swift
+++ SkillMapper/DataController.swift
@@ -230,6 +230,39 @@ class DataController: ObservableObject {
         
         selectedSkill = skill
     }
+    
+    func count<T>(for fetchRequest: NSFetchRequest<T>) -> Int {
+        (try? container.viewContext.count(for: fetchRequest)) ?? 0
+    }
+    
+    func hasEarned(award: Award) -> Bool {
+        switch award.criterion {
+        case "skills":
+            // returns true if they added a certain number of skills
+            let fetchRequest = Skill.fetchRequest()
+            let awardCount = count(for: fetchRequest)
+            return awardCount >= award.value
+
+        case "learned":
+            // returns true if they learned a certain number of skills
+            let fetchRequest = Skill.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "completed = true")
+            let awardCount = count(for: fetchRequest)
+            return awardCount >= award.value
+
+        case "tags":
+            // return true if they created a certain number of tags
+            let fetchRequest = Tag.fetchRequest()
+            let awardCount = count(for: fetchRequest)
+            return awardCount >= award.value
+
+        default:
+            // an unknown award criterion; this should never be allowed
+            // fatalError("Unknown award criterion: \(award.criterion)")
+            return false
+        }
+    }
+
 }
 
 
